47411ba75950638bc9b53e6261741b5640002aee
ulan <ulan.0418@gmail.com>
2021-07-16 07:50:54 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import Header from "./components/Header";
 import Note from "./components/Note";
 import Footer from "./components/Footer";
+import notes from './notes'
 
 let App = () => {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map(note=>{
+        return <Note key={note.key} title={note.title} content = {note.content}/>
+      })}
       <Footer />
     </div>
   );
--- src/components/Note.js
+++ src/components/Note.js
@@ -1,8 +1,8 @@
-let Note = () => {
+let Note = (props) => {
   return (
     <div className="note">
-      <h1>Note title</h1>
-      <p>it is first test note</p>
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
     </div>
   );
 };
